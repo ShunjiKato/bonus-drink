35b3fba8f9497f7fc0eec686eeddedb741ea1ef3
Shunji Kato <ShunjiKato@users.noreply.github.com>
2017-04-23 20:14:03 +0900

    first answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,16 @@
 class BonusDrink
+  GIFT_NUMBER = 3
+
   def self.total_count_for(amount)
-    0
+    total_count = amount
+    surplus = amount
+    while surplus >= GIFT_NUMBER
+      count_array = surplus.divmod(GIFT_NUMBER)
+      surplus = count_array[0] + count_array[1]
+      total_count += count_array[0]
+    end
+    total_count
   end
 end
+
+puts BonusDrink.total_count_for(100)
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -2,4 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  specify { expect(BonusDrink.total_count_for(1)).to eq 1 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(7)).to eq 10 }
+  specify { expect(BonusDrink.total_count_for(8)).to eq 11 }
+  specify { expect(BonusDrink.total_count_for(9)).to eq 13 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(100)).to eq 149 }
 end
